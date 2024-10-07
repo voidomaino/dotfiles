66c266154a065105be62e69c0bf877dc79a8e9ab
Liu Haochen <voidomaino@gmail.com>
2024-10-07 19:35:02 +0800

    misc

--- nvim/lua/plugins/beautify.lua
+++ nvim/lua/plugins/beautify.lua
@@ -5,7 +5,7 @@ return{
         name = "catppuccin", 
         priority = 1000,
         opts = function()
-            vim.cmd.colorscheme "catppuccin-latte"
+            vim.cmd.colorscheme "catppuccin-mocha"
         end
     },
     {
